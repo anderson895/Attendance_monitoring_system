a5edc863012e7cb4c2da70a2b76db22695f48dfe
JARP <andersonandy046@gmail.com>
2024-11-24 13:25:44 +0800

    update DB

--- db/gab_attendance.sql
+++ db/gab_attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2024 at 12:38 PM
+-- Generation Time: Nov 24, 2024 at 06:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,7 +45,12 @@ INSERT INTO `attendance` (`a_id`, `a_student_id`, `a_status`, `a_reason`, `a_dat
 (14, 1, 'Present', NULL, '2024-11-21', 'Pending'),
 (15, 1, 'Present', NULL, '2024-11-20', 'Pending'),
 (16, 3, 'Absent', 'awdawdawd', '2024-11-22', 'Pending'),
-(17, 1, 'Absent', 'I have a sick', '2024-11-23', 'approved');
+(17, 1, 'Absent', 'I have a sick', '2024-11-23', 'Pending'),
+(18, 3, 'Present', NULL, '2024-11-23', 'Approved'),
+(19, 6, 'Present', NULL, '2024-11-23', 'Pending'),
+(20, 3, 'Present', NULL, '2024-11-24', 'Approved'),
+(21, 1, 'Absent', 'may lagnat', '2024-11-24', 'Declined'),
+(22, 25, 'Present', NULL, '2024-11-24', 'Approved');
 
 -- --------------------------------------------------------
 
@@ -69,9 +74,21 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fname`, `mname`, `lname`, `username`, `password`, `role`, `created_at`) VALUES
-(1, 'Gab', '', 'Ackerman', 'student', 'student', 'Student', '2024-11-23 01:13:10'),
+(1, 'Gabi', '', 'Ackerman', 'student', 'student', 'Student', '2024-11-23 12:20:38'),
 (2, 'Joshua', 'Anderson', 'Padilla', 'andy', 'andy', 'Instructor', '2024-11-23 05:25:26'),
-(3, 'April', 'Jane', 'De Leon', 'april', 'april', 'Student', '2024-11-23 02:29:50');
+(3, 'April', 'Jane', 'De Leon', 'april', 'april', 'Student', '2024-11-23 02:29:50'),
+(4, 'juan', 'list', 'time', 'onepeace', '', 'Student', '2024-11-23 15:15:36'),
+(5, 'test', 'testt', 'test', 'test', '', 'Student', '2024-11-23 15:20:31'),
+(6, 'angela', 'denise', 'flores', 'angenise24', 'angenise24', 'Student', '2024-11-23 15:28:55'),
+(8, 'joshua', 'anderson', 'padilla', 'joshua', 'joshua', 'Student', '2024-11-23 16:05:47'),
+(9, 'justin', 'justin', 'justin', 'justin', 'justin', 'Student', '2024-11-23 16:08:15'),
+(10, 'sample', 'sample', 'sample', 'sample', 'sample', 'Student', '2024-11-23 16:11:38'),
+(13, 'q', 'q', 'q', 'q', 'q', 'Student', '2024-11-23 16:15:35'),
+(19, 'n', NULL, 'nn', 'n', 'n', 'Student', '2024-11-24 04:12:01'),
+(21, 'juan1', NULL, 'juanjuan', 'juan', 'juan', 'Student', '2024-11-24 04:29:03'),
+(22, 'QWERT', 'QWERT', 'QWERT', 'QWERT', 'QWERT', 'Student', '2024-11-24 04:01:48'),
+(25, 'pasko', 'pasko', 'pasko', 'pasko', 'pasko', 'Student', '2024-11-24 04:18:08'),
+(26, 'try', 'trytry', 'try', 'try', 'try', 'Student', '2024-11-24 04:27:12');
 
 --
 -- Indexes for dumped tables
@@ -99,13 +116,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Constraints for dumped tables
